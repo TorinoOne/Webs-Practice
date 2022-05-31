ad8ca6365581681b47a2bc30d734c3e20ea74965
unknown <torinoone1996@gmail.com>
2022-05-31 20:27:23 -0300

    31-5-22 20:27

--- Online-Magazine/js/index.js
+++ Online-Magazine/js/index.js
@@ -14,9 +14,31 @@ listElements.forEach(listElement => {
         menu.style.height = `${height}px`
     })
 });
-//_________________________________________________________________________//
+//___________________________________Date-Clock_________________________________________//
+const $tiempo = document.querySelector (`.tiempo`),
+$fecha = document.querySelector(`.fecha`);
 
+function clock () {
+    let f = new Date (),
+    dia = f.getDate (),
+    mes = f.getMonth () + 1,
+    anio = f.getFullYear (),
+    diaSemana = f.getDay ();
 
+    dia = ('0' + dia).slice(-2);
+    mes = ('0' + mes).slice(-2);
+
+    let timeString = f.toLocaleTimeString();
+    $tiempo.innerHTML = timeString;
+
+    let semana = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado']
+    let showSemana = (semana[diaSemana])
+    $fecha.innerHTML = `${anio}-${mes}-${dia} ${showSemana}`
+}
+setInterval(() => {
+    clock();
+
+}, 1000)
 //_________________________________________________________________________//
 
 
